655fac52fb13e9f307c39be4dec575c285649b26
Bas Boot <github@basboot.nl>
2025-01-31 13:22:32 +0100

    Separate check and update

--- main.py
+++ main.py
@@ -24,22 +24,27 @@ def perform_moves(player, state, paths):
         next_states.append(next_state)
     return next_states
 
+def legal_paths(player, state, game):
+    # ignore red texts during rule change
+    if state["rule_change"] and game[state[player]]["is_red"]:
+        return ["yes"]
+    else:
+        if game[state[player]]["look_at"] in {"this", "other"}:
+            # normal rule
+            player_to_look_at = player if game[state[player]]["look_at"] == "this" else OTHER[player]
+            checks_to_do = game[state[player]]["check"].split(", ")
+            is_yes = reduce(lambda x, y: x or y, [game[state[player_to_look_at]][check] for check in checks_to_do])
+            return ["yes"] if is_yes else ["no"]
+        else:
+            # advanced rule
+            return []
+
 def legal_next_states(state, game):
     next_states = []
     for player in ["player1", "player2"]:
-        # ignore red texts during rule change
-        if state["rule_change"] and game[state[player]]["is_red"]:
-            next_states += perform_moves(player, state, ["yes"])
-        else:
-            if game[state[player]]["look_at"] in {"this", "other"}:
-                # normal rule
-                player_to_look_at = player if game[state[player]]["look_at"] == "this" else OTHER[player]
-                checks_to_do = game[state[player]]["check"].split(", ")
-                is_yes = reduce(lambda x, y: x or y, [game[state[player_to_look_at]][check] for check in checks_to_do])
-                next_states += perform_moves(player, state, ["yes"] if is_yes else ["no"])
-            else:
-                # advanced rule
-                pass
+        paths = legal_paths(player, state, game)
+        next_states += perform_moves(player, state, paths)
+
     return next_states
 
 
@@ -58,7 +63,7 @@ if __name__ == '__main__':
     print(game)
 
     state = {
-        "player1": 9,
+        "player1": 1,
         "player2": 2,
         "rule_change": False
     }
